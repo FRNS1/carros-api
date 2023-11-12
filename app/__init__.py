30d11f450a00241e2d23b4106e32bef72291ba5d
FRNS1 <fernandesjoao2009@hotmail.com>
2023-11-12 20:32:25 -0300

    up

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,6 @@ def create_app():
     CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:34215547@carros-usados-db.cgjfydnkuq23.us-east-1.rds.amazonaws.com/carrosusados'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.run(host='172.31.47.123', port=5000)
-    app.run(debug=True)
     db.init_app(app)
     migrate = Migrate(app, db)
     app.register_blueprint(users_bp, url_prefix='/users')
@@ -22,3 +20,6 @@ def create_app():
     app.register_blueprint(files_bp)
 
     return app
+
+if __name__ == '__main__':
+    create_app().run(host='172.31.47.123', port=5000, debug=True)
