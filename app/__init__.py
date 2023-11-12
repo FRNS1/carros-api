06737f77c3203f53e91ad8dd5c5762b1abc0dbbb
FRNS1 <fernandesjoao2009@hotmail.com>
2023-11-12 20:30:51 -0300

    up

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:34215547@carros-usados-db.cgjfydnkuq23.us-east-1.rds.amazonaws.com/carrosusados'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.run(host='172.31.47.123', port=5000)
+    app.run(debug=True)
     db.init_app(app)
     migrate = Migrate(app, db)
     app.register_blueprint(users_bp, url_prefix='/users')
